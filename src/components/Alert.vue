c63ed0cf1f2bcda3bf761b33f0e3f13d0e9f844e
Jemima <contatojemima@gmail.com>
2020-10-01 11:22:59 -0300

    Marcação e estilização (desktop e mobile)
    "FRETE GRATIS PARA TODO O BRASIL \O/"

---
+++ src/components/Alert.vue
@@ -0,0 +1,62 @@
+<template lang="pug">
+  section#Alert
+    h1 Frete grátis para todo o Brasil
+    h4  em compras acima de R$ 200,00
+</template>
+
+<script>
+export default {
+  name: 'Alert'
+}
+</script>
+
+<style lang="scss">
+@import "../styles/variables.scss";
+
+// GENERIC 
+#Alert {
+  background-color: $black;
+  color: white;
+
+  h4 { opacity: .6;}
+}
+
+// MOBILE E TABLET
+@media only screen and (max-width: 900px) {
+  #Alert {
+    text-justify: center;
+    text-align: center;
+    padding: 96px 64px;
+
+    h1 {
+      font-size: 30px;
+      margin-bottom: 40px;
+    }
+
+    h4 {
+      font-size: 25px;
+    }
+  }
+}
+
+// DESKTOP 
+@media only screen and (min-width: 901px) {
+  #Alert {
+    border-radius: 60px;
+
+    text-justify: center;
+    text-align: center;
+
+    padding: 12px 0px;
+    margin: 0px 24px;
+    h1 {
+      font-size: 30px;
+      // margin-bottom: 40px;
+    }
+
+    h4 {
+      font-size: 20px;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,20 @@
     Header
     Banner
     CategoryList
+    Alert
+    
 </template>
 
 <script>
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import CategoryList from '../components/CategoryList'
+import Alert from '../components/Alert'
 import Footer from '../components/Footer'
 
 export default {
   name: 'Home',
-  components: { Header, Banner, CategoryList, Footer }
+  components: { Header, Banner, CategoryList, Alert, Footer }
 }
 </script>
 
